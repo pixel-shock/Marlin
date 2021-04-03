e341071ab24dba28179396ba1fea89815d67c8e0
Tino Wehe <tino.wehe@brandrockers.com>
2021-04-03 10:48:55 +0200

    Readjust Probing count, homing speed, activate LCD status update gcodes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1010,7 +1010,7 @@
  * A total of 2 does fast/slow probes with a weighted average.
  * A total of 3 or more adds more slow probes, taking the average.
  */
-#define MULTIPLE_PROBING 4
+#define MULTIPLE_PROBING 2
 //#define EXTRA_PROBING    1
 
 /**
@@ -1393,7 +1393,7 @@
 #endif
 
 // Homing speeds (mm/min)
-#define HOMING_FEEDRATE_XY (80*60)
+#define HOMING_FEEDRATE_XY (50*60)
 #define HOMING_FEEDRATE_Z  (4*60)
 
 // Validate that endstops are triggered on homing moves
