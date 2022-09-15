08c074d7488c56badaff4af27dedf18f72bed9ca
Tino W <pixel-shock@users.noreply.github.com>
2022-09-15 20:04:20 +0200

    Calibrate Y-Steps from 80 to 81.32

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1165,7 +1165,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 140.46 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 81.32, 400, 140.46 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
