afbeebf9518e9552cb5a5d50bc30d46210f331a9
Tino Wehe <tino.wehe@brandrockers.com>
2022-09-17 14:58:16 +0200

    Disable Linear Advace, Reset Steps for X,Y,Z,E to factory defaults

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1165,7 +1165,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.20, 81.32, 400.80, 140.46 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400.80, 140 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
