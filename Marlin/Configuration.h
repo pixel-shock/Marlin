b4efa6b127c61c385f7532a8ab4e781938d402a6
Tino Wehe <tino.wehe@brandrockers.com>
2022-09-29 13:13:50 +0200

    Fix Z-Probe PIN, fix serial port #2

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -120,7 +120,7 @@
  * Currently Ethernet (-2) is only supported on Teensy 4.1 boards.
  * :[-2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-#define SERIAL_PORT_2 0
+#define SERIAL_PORT_2 1
 //#define BAUDRATE_2 250000   // :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000] Enable to override BAUDRATE
 
 /**
@@ -1291,7 +1291,7 @@
  *      - normally-closed switches to GND and D32.
  *      - normally-open switches to 5V and D32.
  */
-#define Z_MIN_PROBE_PIN P0_10 // Pin 32 is the RAMPS default
+#define Z_MIN_PROBE_PIN PC_13 // Pin 32 is the RAMPS default
 
 /**
  * Probe Type
